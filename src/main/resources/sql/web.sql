aa41cc05d6b86af319e79b2f4b8e6bdf4cafe6a4
yongqian.liu <yongqian.liu@peraglobal.com>
2016-12-12 13:18:47 +0800

    更新：sql 文件

--- src/main/resources/sql/web.sql
+++ src/main/resources/sql/web.sql
@@ -7,7 +7,7 @@ CREATE TABLE `Web` (
   `groupName` varchar(500) NOT NULL DEFAULT '' COMMENT '组名称', 
   `express` varchar(500) NULL DEFAULT '' COMMENT '表达式',
   `state` varchar(500) NULL DEFAULT '' COMMENT '状态',  
-  `createTime` Date NULL COMMENT '创建时间',
-  `updateTime` Date NULL COMMENT '更新时间',
+  `createTime` datetime NULL COMMENT '创建时间',
+  `updateTime` datetime NULL COMMENT '更新时间',
   PRIMARY KEY (`crawlerId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='WEB存储表';
