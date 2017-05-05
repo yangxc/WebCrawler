07f371ec8a2f8c7a39e96f7c54237336b47b8c16
jj <yang@yang.com>
2017-05-05 10:17:07 +0800

    增加了服务注册功能

--- src/main/java/com/peraglobal/Application.java
+++ src/main/java/com/peraglobal/Application.java
@@ -2,6 +2,7 @@ package com.peraglobal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 
 import com.peraglobal.common.CurrentApplicationContext;
@@ -18,6 +19,7 @@ import com.peraglobal.common.CurrentApplicationContext;
  * @version 1.0 2016-12-8 </br>
  * 			最后修改人 无
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
